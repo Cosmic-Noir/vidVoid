170fe9e693b7167929cb5531886be7d32d87cf12
Kristen Doucette <cosmicnoir90@gmail.com>
2019-06-26 20:49:56 -0600

    Add re-call of GET request with updated page number by hitting next to advance

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 // API key
 const apiKey = "41852c5354f2d366f322d470d71ec51f";
 const baseURL = "https://api.themoviedb.org/3/search/";
+let page = 1;
+let currentQuery;
 
 function handleSuggest() {
   // User clicks suggest button to generate a recommended result
-  $('#genRandom').click(function(){
+  $("#genRandom").click(function() {
     console.log("handleSuggest has button has been clicked");
     getSuggestion();
-    // In case they were previously using search, hide the search form when asking for a suggestion 
-    $('.js-search').addClass('hide');
-    $('.js-results').addClass('hidden');
-  })
+    // In case they were previously using search, hide the search form when asking for a suggestion
+    $(".js-search").addClass("hide");
+    $(".js-results").addClass("hidden");
+  });
 }
 
 function handleSearch() {
   // user clickes Search The Void button and reveals the search form options
-  $('#showSearch').click(function(){
-    $('.js-search').removeClass('hidden');
-    $('.js-suggestion').addClass('hidden');
-    
-  })
+  $("#showSearch").click(function() {
+    $(".js-search").removeClass("hidden");
+    $(".js-suggestion").addClass("hidden");
+  });
 }
 
-function getSuggestion(){
+function getSuggestion() {
   // sends GET request to MDB for trending movie/tv media
-  let url = "https://api.themoviedb.org/3/trending/all/week?api_key=41852c5354f2d366f322d470d71ec51f";
+  let url =
+    "https://api.themoviedb.org/3/trending/all/week?api_key=41852c5354f2d366f322d470d71ec51f";
   fetch(url)
     .then(response => {
       if (response.ok) {
@@ -33,37 +35,43 @@ function getSuggestion(){
       throw new Error(response.statustext);
     })
     .then(responseJson => displaySuggestion(responseJson));
-    console.log('getSuggestion has run and should have returned a Json object');
-
+  console.log("getSuggestion has run and should have returned a Json object");
 }
 
-function displaySuggestion(responseJson){
+function displaySuggestion(responseJson) {
   // Should randomly select one result from array and display those details
-  $('.js-suggestion').removeClass('hidden');
-  $('.js-search').addClass('hidden');
+  $(".js-suggestion").removeClass("hidden");
+  $(".js-search").addClass("hidden");
   console.log(responseJson);
-  let randomSelect = Math.floor(Math.random()*20);
+  let randomSelect = Math.floor(Math.random() * 20);
   console.log(randomSelect);
-  $('#suggestedImage').attr("src", `http://image.tmdb.org/t/p/w185${responseJson.results[randomSelect].poster_path}`);
-  $('#suggestedTitle').text(responseJson.results[randomSelect].original_title || responseJson.results[randomSelect].name);
-  $('#suggestedRating').text(responseJson.results[randomSelect].vote_average);
-  $('#suggestedRelease').text(responseJson.results[randomSelect].release_date);
-  $('#suggestedDesc').text(responseJson.results[randomSelect].overview);
-  
-  console.log('displaySuggestion has run');
+  $("#suggestedImage").attr(
+    "src",
+    `http://image.tmdb.org/t/p/w185${
+      responseJson.results[randomSelect].poster_path
+    }`
+  );
+  $("#suggestedTitle").text(
+    responseJson.results[randomSelect].original_title ||
+      responseJson.results[randomSelect].name
+  );
+  $("#suggestedRating").text(responseJson.results[randomSelect].vote_average);
+  $("#suggestedRelease").text(responseJson.results[randomSelect].release_date);
+  $("#suggestedDesc").text(responseJson.results[randomSelect].overview);
 
-
+  console.log("displaySuggestion has run");
 }
 
 function handleSearchForm() {
   // handles search into query from form data
   $("#js-searchForm").submit(event => {
     event.preventDefault();
-
+    page = 1;
     // Convert user input into variable to pass as a param
-    let searchTerm = $("#js-query").val();
+    let query = $("#js-query").val();
+    currentQuery = query;
     console.log("`handleSearchForm()` ran");
-    getMedia(searchTerm);
+    getMedia(query);
   });
 }
 
@@ -79,7 +87,7 @@ function getMedia(query) {
   // Takes keyword from user input
   const params = {
     query: query,
-    page: 1,
+    page: page,
     api_key: apiKey,
     indlucde_adult: false,
     include_video: false,
@@ -116,59 +124,84 @@ function displayResults(responseJson, mediaForm) {
   $("#js-resultNum").text(responseJson.total_results);
 
   // New for loop will add unfilled content to the containing <ul>, identified by JSON ID - Removed inner defaul text from "release date" <h4> due to no population when "person" searched.
-  for (let i = 0; i < responseJson.results.length; i++){
-    $('#js-resultList').append(
+  for (let i = 0; i < responseJson.results.length; i++) {
+    $("#js-resultList").append(
       `<li class="result" id="${responseJson.results[i].id}">
         <img id="img${responseJson.results[i].id}" src=""></img>
         <h3 id="title${responseJson.results[i].id}">TITLE</h3>
         <h4 id="rating${responseJson.results[i].id}">RATING</h4>
         <h4 id="release${responseJson.results[i].id}"></h4>
       </li>`
-    )
+    );
   }
 
   // New if statements will add correct content depending on mediaForm
-  if (mediaForm ==="movie") {
-    for (let i = 0; i < responseJson.results.length; i++){
-      $(`#img${responseJson.results[i].id}`).attr("src", `http://image.tmdb.org/t/p/w185${responseJson.results[i].poster_path}`);
-      $(`#title${responseJson.results[i].id}`).text(`${responseJson.results[i].original_title}`);
-      $(`#rating${responseJson.results[i].id}`).text(`${responseJson.results[i].vote_average}`);
-      $(`#release${responseJson.results[i].id}`).text(`${responseJson.results[i].release_date}`);
+  if (mediaForm === "movie") {
+    for (let i = 0; i < responseJson.results.length; i++) {
+      $(`#img${responseJson.results[i].id}`).attr(
+        "src",
+        `http://image.tmdb.org/t/p/w185${responseJson.results[i].poster_path}`
+      );
+      $(`#title${responseJson.results[i].id}`).text(
+        `${responseJson.results[i].original_title}`
+      );
+      $(`#rating${responseJson.results[i].id}`).text(
+        `${responseJson.results[i].vote_average}`
+      );
+      $(`#release${responseJson.results[i].id}`).text(
+        `${responseJson.results[i].release_date}`
+      );
     }
     console.log("Movie result generation complete");
   }
 
   // If mediaForm === tv
-  if (mediaForm ==="tv") {
-    for (let i = 0; i < responseJson.results.length; i++){
-      $(`#img${responseJson.results[i].id}`).attr("src", `http://image.tmdb.org/t/p/w185${responseJson.results[i].poster_path}`);
-      $(`#title${responseJson.results[i].id}`).text(`${responseJson.results[i].original_name}`);
-      $(`#rating${responseJson.results[i].id}`).text(`${responseJson.results[i].vote_average}`);
-      $(`#release${responseJson.results[i].id}`).text(`${responseJson.results[i].first_air_date}`);
+  if (mediaForm === "tv") {
+    for (let i = 0; i < responseJson.results.length; i++) {
+      $(`#img${responseJson.results[i].id}`).attr(
+        "src",
+        `http://image.tmdb.org/t/p/w185${responseJson.results[i].poster_path}`
+      );
+      $(`#title${responseJson.results[i].id}`).text(
+        `${responseJson.results[i].original_name}`
+      );
+      $(`#rating${responseJson.results[i].id}`).text(
+        `${responseJson.results[i].vote_average}`
+      );
+      $(`#release${responseJson.results[i].id}`).text(
+        `${responseJson.results[i].first_air_date}`
+      );
     }
     console.log("TV result generation complete");
   }
 
-   // If mediaForm === person
-   if (mediaForm ==="person") {
-    for (let i = 0; i < responseJson.results.length; i++){
-      $(`#img${responseJson.results[i].id}`).attr("src", `http://image.tmdb.org/t/p/w185${responseJson.results[i].profile_path}`);
-      $(`#title${responseJson.results[i].id}`).text(`${responseJson.results[i].name}`);
-      $(`#rating${responseJson.results[i].id}`).text(`${responseJson.results[i].popularity}`);
+  // If mediaForm === person
+  if (mediaForm === "person") {
+    for (let i = 0; i < responseJson.results.length; i++) {
+      $(`#img${responseJson.results[i].id}`).attr(
+        "src",
+        `http://image.tmdb.org/t/p/w185${responseJson.results[i].profile_path}`
+      );
+      $(`#title${responseJson.results[i].id}`).text(
+        `${responseJson.results[i].name}`
+      );
+      $(`#rating${responseJson.results[i].id}`).text(
+        `${responseJson.results[i].popularity}`
+      );
     }
     console.log("Person result generation complete");
   }
 
-  // If no image is located for specified title. But doesn't work. 
-//   if (!responseJson.results[i].poster_path) {
-//   $(`#img${responseJson.results[i].id}`).replaceWith(`
-//   <img src="missingImage.jpeg">`);
-//   console.log(
-//     `${
-//       responseJson.results[i].id
-//     } img element replaced due to missing poster`
-//   );
-// }
+  // If no image is located for specified title. But doesn't work.
+  //   if (!responseJson.results[i].poster_path) {
+  //   $(`#img${responseJson.results[i].id}`).replaceWith(`
+  //   <img src="missingImage.jpeg">`);
+  //   console.log(
+  //     `${
+  //       responseJson.results[i].id
+  //     } img element replaced due to missing poster`
+  //   );
+  // }
 
   createNext(responseJson);
   handleResultSelect(responseJson);
@@ -178,14 +211,23 @@ function displayResults(responseJson, mediaForm) {
 
 function createNext(responseJson) {
   // First create the button if the results are more than 20
-  if (responseJson.total_results - 20 > 20) {
+  if (responseJson.total_results > 20) {
     $(".js-results").append(`
     <button id="next">Next</button>`);
     console.log("createNext ran");
   } else {
     $("#next").remove();
   }
+  handleNext();
   // displayed when user displays results, allows new GET request for page 2
+}
+
+function handleNext() {
+  // user clicks next, page value increased by 1
+  $("#next").click(function() {
+    page += 1;
+    getMedia(currentQuery);
+  });
 }
 
 // Need to find a way to pass correct movie ID to use in new GET request - UNTESTED
@@ -195,7 +237,7 @@ function handleResultSelect(responseJson) {
     console.log("handleResultSelect ran phase 1: hide results list");
     $(".js-details").removeClass("hidden");
     getSingleResult();
-    handleBackResults();
+    handleBackToResults();
   });
 
   console.log("handleResultSelect ran");
@@ -203,7 +245,7 @@ function handleResultSelect(responseJson) {
 
 function getSingleResult() {
   // let mediaID = 9836; // Default test value - works
-  
+
   let url = `https://api.themoviedb.org/3/movie/${mediaID}?api_key=${apiKey}`;
   console.log(url);
   fetch(url)
@@ -214,10 +256,10 @@ function getSingleResult() {
       throw new Error(response.statustext);
     })
     .then(responseJson => console.log(responseJson));
-    // Must replace above with call to displayDetails(responseJson) - but unsure if still working after changes made without testing due to internet outage. 
+  // Must replace above with call to displayDetails(responseJson) - but unsure if still working after changes made without testing due to internet outage.
 }
 
-function handleBackResults() {
+function handleBackToResults() {
   // Unhides the results div and hides the details div
   $("#backResults").click(function() {
     $(".js-details").addClass("hidden");
@@ -227,8 +269,6 @@ function handleBackResults() {
 
 function displayDetails(responseJson) {
   // Displays details about single selected result
-
-
 }
 
 console.log("VidVoid App Active");
